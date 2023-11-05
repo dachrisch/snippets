624b5797fcb3e687e18e72053400cd130accfdd7
dachrisch <christian.daehn@gmail.com>
2023-11-05 14:40:46 +0100

    Refactor PleromaVersions class and add tests

--- pleroma_versions.py
+++ pleroma_versions.py
@@ -2,7 +2,7 @@ import concurrent
 import concurrent.futures
 import json
 from datetime import datetime
-from typing import Tuple, List
+from typing import Tuple, List, Any, Sequence, Callable
 from urllib.parse import urljoin
 from zoneinfo import ZoneInfo
 
@@ -13,14 +13,28 @@ from requests import Response
 from rich import print
 
 
+class LongAsyncTaskExecutor(object):
+    def __init__(self, max_workers: int = 5):
+        self.max_workers = max_workers
+
+    def execute(self, items: Sequence[Any], item_func: Callable, item_func_args: Sequence[Any],
+                callback: Callable[[Any], Any]):
+        with alive_bar(len(items)) as bar:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+                futures = [executor.submit(item_func, *item_func_args, item) for item in items]
+
+                for future in concurrent.futures.as_completed(futures):
+                    callback(future.result())
+                    bar()
+
+
 class PleromaVersions(object):
 
     def __init__(self, project_id: int, repository_id: int, max_workers: int = 5):
-        self.max_workers = max_workers
+        self.executor = LongAsyncTaskExecutor(max_workers)
         self.repository_url = (f'https://git.pleroma.social/api/v4/projects/{project_id}'
                                f'/registry/repositories/{repository_id}/tags/'
                                )
-        print(f'Getting all pages from [[bold yellow]{self.repository_url}[/bold yellow]]')
 
     def list(self, after_date: str):
         if not self.repository_url:
@@ -28,31 +42,30 @@ class PleromaVersions(object):
 
         filter_after_date = datetime.strptime(after_date, '%Y-%m-%d').astimezone(ZoneInfo('Europe/Berlin'))
 
+        tags = self._get_all_tags()
+        filtered_tags = self._get_all_tag_details(tags, filter_after_date)
+
+        print(filtered_tags)
+
+    def _get_all_tag_details(self, tags: Sequence, filter_after_date: datetime) -> Sequence:
+        print(f'Getting details for [bold yellow]{len(tags)} tags')
+        tags_details = []
+        self.executor.execute(items=tags, item_func=self._tag_details, item_func_args=(),
+                              callback=lambda result: tags_details.append(result))
+        filtered_tags = list(
+            filter(lambda tag: filter_after_date < datetime.fromisoformat(tag['created_at']), tags_details))
+        return filtered_tags
+
+    def _get_all_tags(self) -> Sequence:
+        print(f'Getting all pages from [[bold yellow]{self.repository_url}[/bold yellow]]')
         response, tags = self._get_tags(self.repository_url)
         current_page = int(response.headers.get('X-Page'))
         total_pages = int(response.headers.get('X-Total-Pages'))
-
-        with alive_bar(total_pages) as bar:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-                tag_futures = [executor.submit(self._get_tags, self.repository_url, page) for page in
-                               range(current_page, total_pages + 1)]
-
-                for tag_future in concurrent.futures.as_completed(tag_futures):
-                    tags.extend(tag_future.result()[1])
-                    bar()
-
-        print(f'Getting details for [bold yellow]{len(tags)} tags')
-
-        tags_details = []
-        with alive_bar(len(tags)) as bar:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-                tag_futures = [executor.submit(self._tag_details, tag) for tag in tags]
-                for tag_future in concurrent.futures.as_completed(tag_futures):
-                    tags_details.append(tag_future.result())
-                    bar()
-        filtered_tags = list(
-            filter(lambda tag: filter_after_date < datetime.fromisoformat(tag['created_at']), tags_details))
-        print(filtered_tags)
+        self.executor.execute(items=range(current_page + 1, total_pages + 1),
+                              item_func=self._get_tags,
+                              item_func_args=(self.repository_url,),
+                              callback=lambda result: tags.extend(result[1]))
+        return tags
 
     def _get_tags(self, base_url: str, page: int = 1) -> Tuple[Response, List]:
         page_url = base_url + f'?page={page}'
---
+++ tests/test_pleroma_versions.py
@@ -0,0 +1,70 @@
+import unittest
+from datetime import datetime
+from unittest.mock import patch, MagicMock
+from zoneinfo import ZoneInfo
+
+from more_itertools import one
+
+from pleroma_versions import PleromaVersions
+
+
+class TestPleromaVersions(unittest.TestCase):
+    project_id = 123
+    repository_id = 456
+    max_workers = 5
+
+    def setUp(self):
+        self.pv = PleromaVersions(project_id=self.project_id, repository_id=self.repository_id,
+                                  max_workers=self.max_workers)
+
+    @patch('pleroma_versions.requests.get')
+    def test_get_all_tags_successful(self, mock_get):
+        # Mock the response from requests.get
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.headers = {'X-Page': '1', 'X-Total-Pages': '1'}
+        mock_response.content = b'[{"name": "tag1", "created_at": "2020-01-01T00:00:00Z"}]'
+        mock_get.return_value = mock_response
+
+        tags = self.pv._get_all_tags()
+
+        # Assert that requests.get was called with the expected URL
+        mock_get.assert_called_with(
+            f'https://git.pleroma.social/api/v4/projects/{self.project_id}/registry/repositories/{self.repository_id}/tags/?page=1')
+
+        # Assert that the tags were correctly retrieved
+        self.assertEqual(tags, [{"name": "tag1", "created_at": "2020-01-01T00:00:00Z"}])
+
+    @patch('pleroma_versions.LongAsyncTaskExecutor.execute')
+    def test_get_all_tag_details(self, mock_execute):
+        # Prepare a mock for the tags that would be passed to the executor
+        mock_tags = [{'name': 'tag1'},
+                     {'name': 'tag2'}]
+
+        # Mock tag details that would be returned by the item function
+        mock_tag_details = [{'name': 'tag1', 'created_at': '2023-10-01T22:52:53.372+00:00'},
+                            {'name': 'tag2', 'created_at': '2023-09-01T22:52:53.372+00:00'}]
+
+        # Mock the callback inside the LongAsyncTaskExecutor.execute method
+        def mock_execute_callback(items, item_func, item_func_args, callback):
+            for tag in items:
+                # Simulate the item function returning tag details
+                callback(one(filter(lambda i: i['name'] == tag['name'], mock_tag_details)))
+
+        # Set the side effect of the mock execute to our mock callback function
+        mock_execute.side_effect = mock_execute_callback
+
+        # Run the method under test
+        filter_after_date = datetime(2023, 9, 5, tzinfo=ZoneInfo('Europe/Berlin'))
+        filtered_tags = self.pv._get_all_tag_details(mock_tags, filter_after_date)
+
+        # Assert that the execute method was called on the executor
+        mock_execute.assert_called()
+
+        # Assert that only tags after the filter date are included
+        self.assertEqual(1, len(filtered_tags))
+        self.assertEqual(filtered_tags[0]['name'], 'tag1')
+
+
+if __name__ == '__main__':
+    unittest.main()
