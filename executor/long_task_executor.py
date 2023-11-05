aa35460b7825f45cd0e7b819c2371d263386cc51
dachrisch <christian.daehn@gmail.com>
2023-11-05 15:23:04 +0100

    Refactor LongAsyncTaskExecutor to use generics for item_func and callback

--- executor/long_task_executor.py
+++ executor/long_task_executor.py
@@ -1,19 +1,26 @@
 import concurrent
-from typing import Sequence, Any, Callable
+from concurrent import futures
+from typing import Sequence, Any, Callable, TypeVar, Generic
 
 from alive_progress import alive_bar
 
+T = TypeVar('T')
+V = TypeVar('V')
 
-class LongAsyncTaskExecutor(object):
+
+class LongAsyncTaskExecutor(Generic[T]):
     def __init__(self, max_workers: int = 5):
         self.max_workers = max_workers
 
-    def execute(self, items: Sequence[Any], item_func: Callable, item_func_args: Sequence[Any],
-                callback: Callable[[Any], Any]):
+    def execute(self,
+                items: Sequence[T],
+                item_func: Callable[[*Any, T], V],
+                item_func_args: Sequence[Any],
+                callback: Callable[[V], Any]) -> None:
         with alive_bar(len(items)) as bar:
             with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-                futures = [executor.submit(item_func, *item_func_args, item) for item in items]
+                _futures = [executor.submit(item_func, *item_func_args, item) for item in items]
 
-                for future in concurrent.futures.as_completed(futures):
+                for future in concurrent.futures.as_completed(_futures):
                     callback(future.result())
                     bar()
